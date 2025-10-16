9fd98d452b3d32d52076c12301e9b7e7df6ac2c6
Josue <josuelml28@hotmail.com>
2025-10-16 07:34:33 -0500

    Se aisla codigo php y un poco de mejora de visualizacion

--- index.php
+++ index.php
@@ -1,14 +1,18 @@
+<?php include 'funciones.php' ?>
+
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
 </head>
 <body>
-<h2>📝 Formulario de contacto</h2>
+<h2>Formulario de contacto</h2>
 
-<form action="<?= $_SERVER["PHP_SELF"] ?> " method="post">
+<form action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
     <label for="nombre">👤 Nombre:</label><br>
     <input type="text" id="nombre" name="nombre" required><br><br>
 
@@ -18,12 +22,11 @@
     <label for="numero">📱 Numero:</label><br>
     <input type="number" id="numero" name="numero" required><br><br>
 
-
     <input type="submit" value="Enviar">
     <input type="reset" value="Limpiar">
 </form>
-
 <?php
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nombre = htmlspecialchars($_POST["nombre"]);
     $email = htmlspecialchars($_POST["email"]);
@@ -32,43 +35,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $archivo_csv = fopen("datos.csv", "a");
 
     fputcsv($archivo_csv, [$nombre, $email, $numero]);
-
     fclose($archivo_csv);
-
 }
 ?>
 
 
-<table>
+<input class="btn btn-primary" type="button" value="Editar">
+<input class="btn btn-primary" type="submit" value="Eliminar">
+
+<table class="table table-hover">
     <!-- Cabeceras -->
     <thead>
     <tr>
-        <th>👤 Nombre</th>
-        <th>📧 Correo</th>
-        <th>📞 Teléfono</th>
+        <th scope="col">👤 Nombre</th>
+        <th scope="col">📧 Correo</th>
+        <th scope="col">📞 Teléfono</th>
     </tr>
     </thead>
     <tbody>
     <!-- Aquí irían tus datos -->
-    <?php
-    $archivo_csv = fopen("datos.csv", "r");
+    <?php ingresar_contacto(); ?>
 
-    while (($datos = fgetcsv($archivo_csv)) !== false) {
-        echo "<tr>";
-        foreach ($datos as $dato) {
-            echo "<td>" . $dato . "</td>";
-        }
-        echo "</tr>";
-    }
-
-    ?>
 
     </tbody>
 </table>
 
-<div>
 
-</div>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
